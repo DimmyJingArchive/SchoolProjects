b93d799f956fa5a4571860a1de2cf23c2d42626d
PotatosFish <jimmyguding@gmail.com>
2018-09-21 16:35:31 -0500

    thing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<HomePage/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		HomePage
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	background-image: url("./assets/FlagEdited.jpg");
+	background-size: cover;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	padding-top: 60px;
+	padding: 120px;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="HomePage">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomePage',
+	data: function() {
+		return {
+			msg: 'There is no place on earth that remain untouched from the influence of America. As one of the strongest countries in the world, it wields incredible powers and opportunities. But behind the superfluous exaggerations by other Americans, there lies the true reason for which it has grown by. Benjamin Franklin started his career without a dime in his pocket, growing to unimaginable heights. George Washington went from a farmer to the founding father of America. Those achievements were inconceivable everywhere else in the world because of caste systems and royal families. Truth is, the only trait that matters is not the royal status, not the glaring accomplishments made by your ancestors, but your usefulness to the society.'
+		};
+	},
+	filters: {
+	}
+}
+</script>
+
+<style scoped>
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
